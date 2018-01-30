38d11b0858ded4dcc1be5950aa7d622ecfd1a6bf
Dominic Brown <yeahdom@gmail.com>
2018-01-29 18:04:25 -0800

    add a faster solution to #7

--- 7_10001st_prime_js/euler.js
+++ 7_10001st_prime_js/euler.js
@@ -4,56 +4,111 @@
 //
 // What is the 10 001st prime number?
 
-var userNum;
-var primes;
+// first solution
 
-function buildBase(top) {
-  var base = [];
-  for (var i=1;i<top;i++) {
-    base.push(i);
-  }
-  return base;
-}
+// var userNum;
+// var primes;
+//
+// function buildBase(top) {
+//   var base = [];
+//   for (var i=1;i<top;i++) {
+//     base.push(i);
+//   }
+//   return base;
+// }
+//
+// function sieve(arr) {
+//   for (i = 2; i < userNum ; i += 1) {
+//     for (j = i+i ; j <= userNum ; j = j + i) {
+//       arr.splice(arr.indexOf(j),1,"-");
+//     }
+//   }
+//   var filteredArr = arr.filter(item => item !== "-");
+//   return filteredArr;
+// }
+//
+// $(document).ready(function() {
+//   $("#mainform").submit(function(event){
+//     event.preventDefault();
+//
+//     var startTime = performance.now();
+//
+//     // grab user's input
+//     userNum = parseInt($("#input1").val());
+//
+//     var primes = sieve(buildBase(userNum));
+//     var timeTaken = performance.now() - startTime;
+//
+//     // clear output
+//     $("#numberofprimes").text("");
+//     $("#primelist").text("");
+//     // show new output
+//     console.log('10000 prime = ', primes[10000]);
+//     console.log('10001 prime = ', primes[10001]);
+//     console.log('10002 prime = ', primes[10002]);
+//     // 10000 prime =  104729
+//     // 10001 prime =  104743
+//     // 10002 prime =  104759
+//     $("#numberofprimes").append(primes.length);
+//     $("#primelist").append(primes.join('  '));
+//     console.log('time taken = ' + timeTaken.toFixed(2) + ' ms');
+//
+//   });
+// });
 
-function sieve(arr) {
-  for (i = 2; i < userNum ; i += 1) {
-    for (j = i+i ; j <= userNum ; j = j + i) {
-      arr.splice(arr.indexOf(j),1,"-");
-    }
-  }
-  var filteredArr = arr.filter(item => item !== "-");
-  return filteredArr;
+
+// answer
+// 10001 prime =  104743
+// takes about 4 mintues with my version of the sieve method
+
+
+// another method
+
+
+
+function isPrime(num) {
+    if (num == 2 || num == 3) return true;
+    if (num % 2 == 0) return false;
+
+    for (var i = 3; i <= Math.sqrt(num); i += 2)
+        if (num % i == 0)
+            return false;
+
+    return num > 1;
 }
 
 $(document).ready(function() {
   $("#mainform").submit(function(event){
     event.preventDefault();
 
-    var startTime = performance.now();
-
-    // grab user's input
-    userNum = parseInt($("#input1").val());
-
-    var primes = sieve(buildBase(userNum));
-    var timeTaken = performance.now() - startTime;
-
     // clear output
-    $("#numberofprimes").text("");
-    $("#primelist").text("");
-    // show new output
-    console.log('10000 prime = ', primes[10000]);
-    console.log('10001 prime = ', primes[10001]);
-    console.log('10002 prime = ', primes[10002]);
-    // 10000 prime =  104729
-    // 10001 prime =  104743
-    // 10002 prime =  104759
-    $("#numberofprimes").append(primes.length);
-    $("#primelist").append(primes.join('  '));
-    console.log('time taken = ' + timeTaken.toFixed(2) + ' ms');
+    $("#numberofprimes").text(" ");
+    $("#primelist").text(" ");
+
+    var startTime = performance.now();
+    var prime = undefined;
+
+    // grab user's input, this is the Nth prime to find counting from 1
+    var userNum = parseInt($("#input1").val());
+
+    var count = 0;
+    var number = 2;
+
+    while (true) {
+        if (isPrime(number)) {
+            count++
+            if (count == userNum) {
+                prime = number
+                break;
+            }
+        }
+        number++;
+    }
+
+    var timeTaken = ( performance.now() - startTime ).toFixed(2);
+
+    $("#numberofprimes").append('the '+ userNum + 'th prime = '+ prime);
+    $("#primelist").append('time taken = ' + timeTaken + ' ms');
 
   });
 });
-
-
-// answer
-// 10001 prime =  104743
